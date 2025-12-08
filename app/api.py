40d995adfadc571e64783837fbd7ad385fbaac07
damiandm-spec <damiandm@umich.edu>
2025-12-07 19:02:35 -0500

    Update api.py Changed import from VideoSummarizer to MobileSummarizer

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from VideoSummarizer import VideoSummarizer
+from MobileSummarizer import VideoSummarizer
 from dotenv import load_dotenv
 import os
 
